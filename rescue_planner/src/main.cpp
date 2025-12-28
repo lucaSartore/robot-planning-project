9afbd83a14880cde767737ae1f89852c6b6862a2
Luca Sartore <lucasartore02@gmail.com>
2025-12-28 16:19:18 +0100

    able to read all ros topics in ros

--- rescue_planner/src/main.cpp
+++ rescue_planner/src/main.cpp
@@ -2,6 +2,12 @@
 #include <iostream>
 #include "std_msgs/String.h"
 
+
+#include "geometry_msgs/Point32.h"
+#include "geometry_msgs/Polygon.h"
+#include "geometry_msgs/PolygonStamped.h"
+#include "geometry_msgs/Pose.h"
+
 #include "obstacles_msgs/ObstacleArrayMsg.h"
 #include "obstacles_msgs/ObstacleMsg.h"
 
@@ -10,10 +16,17 @@
 
 using namespace std;
 
+
+void exitPointCallback(const geometry_msgs::Polygon & msg) {
+    ROS_INFO("I read a poligon with [%ld] points", msg.points.size());
+}
+
 // void chatterCallback(const std_msgs::String::ConstPtr& msg)
-void chatterCallback(const obstacles_msgs::ObstacleArrayMsg & msg)
-{
-    cout << msg.obstacles.size() << endl;
+void mapCallback(const geometry_msgs::Polygon & msg) {
+    ROS_INFO("I read a poligon with [%ld] points", msg.points.size());
+}
+
+void chatterCallback(const obstacles_msgs::ObstacleArrayMsg & msg) {
     ROS_INFO("I read [%ld] obstacles", msg.obstacles.size());
 }
 
@@ -23,7 +36,10 @@ int main(int argc, char** argv){
     
     ros::NodeHandle n;
     
-    ros::Subscriber sub = n.subscribe("/obstacles", 1000, chatterCallback);
+    // ros::Subscriber sub = n.subscribe("/obstacles", 1000, chatterCallback);
+    // ros::Subscriber sub = n.subscribe("/victims", 1000, chatterCallback);
+    // ros::Subscriber sub = n.subscribe("/map_borders", 1000, mapCallback);
+    ros::Subscriber sub = n.subscribe("/gates", 1000, exitPointCallback);
     
     ros::spin();
     // Obstacle obstacle;
