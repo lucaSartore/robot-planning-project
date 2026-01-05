3a676ba2ee3a47bed562a1fe5fc15d7b123c0148
Luca Sartore <lucasartore02@gmail.com>
2026-01-05 15:57:03 +0100

    add constants file

---
+++ rescue_planner/src/util/constants.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+/// max velocity of the robot
+const float VELOCITY = 1.0;
+/// 1/robot turning radius
+const float ROBOT_K = 2.0;
+/// max time to evaquate
+const float MAX_TIME = 2.0;
+/// robot radius used for collision detection
+const float ROBOT_RADIUS = 0.5;
+/// resolution used in the occupation approximation
+const int OCCUPATION_RESOLUTION_X = 1000;
