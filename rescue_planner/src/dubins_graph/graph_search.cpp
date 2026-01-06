74aa43ef875faff56498a93ac25a040454a7d6c7
Luca Sartore <lucasartore02@gmail.com>
2026-01-06 09:54:08 +0100

    add graph search

---
+++ rescue_planner/src/dubins_graph/graph_search.cpp
@@ -0,0 +1,150 @@
+#include <algorithm>
+
+#include "dubins_graph.hpp"
+#include "../trajectory_planner/trajectory_planner.hpp"
+#include <unordered_map>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+#include "../triangulation/triangulation.hpp"
+#include "../util/hashable_tuple.hpp"
+
+class GraphSearch {
+public:
+    GraphSearch(DubinsGraph& graph, vector<int> victims);
+    vector<ExecutableDubinsTrajectory> execute();
+private:
+    /// graph
+    DubinsGraph & graph;
+    /// initial node
+    DubinsNode * start;
+    /// list of nodes to visit (victims and finally exit)
+    vector<DubinsNode *> to_visit;
+    /// costs associated with reaching the end
+    /// starting from one of the nodes in "to_visit"
+    unordered_map<int, float> heuristic_costs;
+    float heuristic_cost(int node_start, int next_objective);
+
+};
+
+
+/// class used in the priority queue
+/// during the graph search
+class ProgressPoint {
+public:
+    DubinsNode * node;
+    float angle;
+    float length_so_far;
+    float total_heuristic_cost;
+    vector<int> objectives;
+
+    ProgressPoint(DubinsNode * node, float angle, float length_so_far, float total_heuristic_cost, vector<int> objectives) {
+        this->node = node;
+        this->angle = angle;
+        this->length_so_far = length_so_far;
+        this->total_heuristic_cost = total_heuristic_cost;
+        this->objectives = std::move(objectives);
+    }
+
+    bool operator<(ProgressPoint const& other) const {
+        return total_heuristic_cost < other.total_heuristic_cost;
+    }
+};
+
+
+float GraphSearch::heuristic_cost(int node_start, int next_objective) {
+    return distance(
+        graph.graph.nodes[node_start].value,
+        graph.graph.nodes[next_objective].value
+    ) + heuristic_costs[next_objective];
+}
+
+GraphSearch::GraphSearch(DubinsGraph &graph, vector<int> victims): graph(graph) {
+    start = &graph.nodes[graph.graph.robot_position];
+    to_visit = {};
+    for (int v: victims) {
+        to_visit.push_back(&graph.nodes[v]);
+    }
+    to_visit.push_back(&graph.nodes[graph.graph.exit_node]);
+
+    heuristic_costs = {};
+    heuristic_costs[graph.graph.exit_node] = 0;
+
+    // reverse the victims to calculate heuristic cost backword
+    int v_prev = graph.graph.exit_node;
+    std::reverse(victims.begin(), victims.end());
+    for (int v: victims) {
+        heuristic_costs[v] = heuristic_cost(v, v_prev);
+        v_prev = v;
+    }
+}
+
+
+vector<ExecutableDubinsTrajectory> GraphSearch::execute() {
+    unordered_set<tuple<int, float>, hash_duplet> visited;
+
+    vector<int> objectives = {};
+    objectives.reserve(to_visit.size());
+    for (auto v: to_visit) {
+        objectives.push_back(v->id);
+    }
+
+    set<ProgressPoint> nodes_queue = {ProgressPoint(
+        start,
+        graph.map.robot_position.orientation,
+        0,
+        heuristic_cost(graph.graph.robot_position, to_visit[0]->id),
+        objectives
+    )};
+
+    while (!nodes_queue.empty()) {
+        auto to_expand = *nodes_queue.begin();
+        nodes_queue.erase(nodes_queue.begin());
+
+        bool is_next_objective = to_expand.node->id == to_expand.objectives[0];
+
+        if (!is_next_objective) {
+            bool is_other_objective = find(
+                to_expand.objectives.begin(),
+                to_expand.objectives.end(),
+                to_expand.node->id
+            ) == to_expand.objectives.end();
+            // we want to force the correct order
+            if (is_other_objective) {
+                continue;
+            }
+        }
+
+        tuple<int, float> node_identifier = {to_expand.node->id, to_expand.angle};
+
+        // node already visited... don't do anything
+        if (visited.find(node_identifier) != visited.end()) {
+            continue;
+        }
+
+        visited.insert(node_identifier);
+
+        // creating the new objectives list
+        vector<int> new_objectives = vector<int>(to_expand.objectives.begin(), to_expand.objectives.end());
+        if (is_next_objective) {
+            new_objectives.erase(new_objectives.begin());
+        }
+
+        // solution found!
+        if (!new_objectives.empty()) {
+            std::cout << "found solution with final cost: " << to_expand.length_so_far << std::endl;
+            break;
+        }
+
+        for (auto adj: to_expand.node->connections[to_expand.angle]) {
+            float length_so_far = to_expand.length_so_far + adj.trajectory.length;
+            nodes_queue.insert(ProgressPoint(
+                adj.node,
+                adj.arriving_angle,
+                length_so_far,
+                length_so_far + heuristic_cost(adj.node->id, new_objectives[0]),
+                new_objectives
+            ));
+        }
+    }
+}
--- rescue_planner/src/util/hashable_tuple.hpp
+++ rescue_planner/src/util/hashable_tuple.hpp
@@ -6,21 +6,30 @@ using namespace  std;
 struct hash_duplet {
     template <class T1, class T2>
     size_t operator()(const tuple<T1, T2> &x) const {
-        return get<0>(x) ^ (get<1>(x) << 1);
+        auto h0 = hash<T1>{}(std::get<0>(x));
+        auto h1 = hash<T2>{}(std::get<1>(x));
+        return h0 ^ (h1 << 1);
     }
 };
 
 struct hash_triplet {
     template <class T1, class T2, class T3>
     size_t operator()(const tuple<T1, T2, T3> &x) const {
-        return get<0>(x) ^ (get<1>(x) << 1) ^ (get<2>(x) << 2);
+        auto h0 = hash<T1>{}(std::get<0>(x));
+        auto h1 = hash<T2>{}(std::get<1>(x));
+        auto h2 = hash<T3>{}(std::get<2>(x));
+        return h0 ^ (h1 << 1) ^ (h2 << 2);
     }
 };
 
 struct hash_quadruplet {
     template <class T1, class T2, class T3, class T4>
     size_t operator()( const tuple<T1, T2, T3, T4>& x) const {
-        return get<0>(x) ^ (get<1>(x) << 1) ^ (get<2>(x) << 2) ^ (get<3>(x) << 3);
+        auto h0 = hash<T1>{}(std::get<0>(x));
+        auto h1 = hash<T2>{}(std::get<1>(x));
+        auto h2 = hash<T3>{}(std::get<2>(x));
+        auto h3 = hash<T4>{}(std::get<3>(x));
+        return h0 ^ (h1 << 1) ^ (h2 << 2) ^ (h3 << 3);
     }
 };
 
